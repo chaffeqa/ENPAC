1c7a8ecf142876b979a7487070e3cb70127003d8
macattack2k <macklin@goldenorbventure.com>
2011-02-17 12:54:56 -0500

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,3 @@ enpac_site = Site.create(:subdomain => 'www')
 inventory_node = Node.create(:menu_name => 'Inventory', :title => 'Inventory', :shortcut => 'inventory', :displayed => true)
 inventory_node.save
 
-inventory_categories = Category.create([
-    { :title => 'Overpacks / Salvage Drums', :node_attributes=>{:menu_name => 'Special Offers', :title => 'Special Offers', :shortcut => 'Special-Offers', :displayed => true} },
