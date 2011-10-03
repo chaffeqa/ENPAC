0856d0da07a6565848d3af39051edcae4750230c
Quinn <chaffeqa@gmail.com>
2011-10-03 11:02:04 -0400

    turned on frag and model caching

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 SITE_NAME = {"www" => "PhusionCMS", "enpac" => "ENPAC"}
 
-MODEL_CACHING = false
+MODEL_CACHING = true
 PAGE_MEMCACHE_CACHING = false
-VIEW_FRAGMENT_CACHING = false
+VIEW_FRAGMENT_CACHING = true
 
 # Initialize the rails application
 ENPAC::Application.initialize!
