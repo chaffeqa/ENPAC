b8e607c57b104d04dd159a836e43393125493fc9
Macklin Chaffee <macklin@goldenorbventure.com>
2013-02-25 21:17:46 -0700

    updated secret session

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '98d1405sd4063992d94eed8c366564e236c8ebd07cd7440c0b412a40f45006b16ebc1c3a9fbe6b5627ddec9a242aae6d80713f34933469a0934eae01f4167db1'
+Rails.application.config.secret_token = '98d1405sd4063992d94eed8c366564e236c8ebd07cd7440c0b412a40f45006b16ebc1c3a9fbe6b5627ddec9a242aae6d80713f34933469a0934eae01f4167db2'
